56dad1e57a4ea34950ac3dcf9e8b15f6ead7feb5
Your New GitHub Name <your-new-github-email@example.com>
2026-05-21 14:56:58 +1000

    restore dashboard layout alignment

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,5 @@
 ﻿import EasyPipsShell from "../components/EasyPipsShell";
-import AIHeroBanner from "../components/AIHeroBanner";
 
 export default function Page() {
-  return (
-    <>
-      <div className="xl:pl-[280px] px-5 pt-5">
-        <AIHeroBanner />
-      </div>
-
-      <EasyPipsShell page="dashboard" />
-    </>
-  );
+  return <EasyPipsShell page="dashboard" />;
 }
